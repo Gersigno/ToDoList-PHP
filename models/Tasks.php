dfab1cee129e20f95ee246c4a6a285dd032af509
Gersigno <gersand.demaline@gmail.com>
2024-07-23 10:32:21 +0200

    Query parameters bind & getExpiration implementation

--- models/Tasks.php
+++ models/Tasks.php
@@ -19,8 +19,15 @@ class Tasks extends Model {
      * @param String Date-Time type formated string, empty for null.
      */
     public function createNewTask($title, $description, $expiration) {
-        $expiration == "" ? $expiration = "NULL" : $expiration = "\"" . str_replace("T", " ", $expiration) . "\"";
-        $this->runQuery("INSERT INTO `$this->table` (`owner_id`, `title`, `description`, `done`, `creation_date`, `expiration_date`) VALUES (" . $_SESSION['userId'] . ", \"$title\", \"$description\", 0, \"" . date_create('now')->format('y-m-d H:i:s') . "\", $expiration)");
+        $expiration == "" ? $expiration = NULL : $expiration = str_replace("T", " ", $expiration);
+        
+        $this->runQuery("INSERT INTO `$this->table` (`owner_id`, `title`, `description`, `done`, `creation_date`, `expiration_date`) VALUES (:owner_id, :title, :description, 0, :creation_date, :expiration)", [
+            ':owner_id' => $_SESSION['userId'],
+            ':title' => $title,
+            ':description' => $description,
+            ':creation_date' => date_create('now')->format('y-m-d H:i:s'),
+            ':expiration' => $expiration
+        ]);
     }
 
     /**
@@ -31,8 +38,14 @@ class Tasks extends Model {
      * @param String Date-Time type formated string, empty for null.
      */
     public function editTask($task_id, $task_title, $description, $expiration) {
-        $expiration == "" ? $expiration = "NULL" : $expiration = "\"" . str_replace("T", " ", $expiration) . "\"";
-        $this->runQuery("UPDATE `$this->table` SET `title`=\"" . $task_title . "\", `description`=\"" . $description . "\", `expiration_date`=$expiration WHERE `id`=" . $task_id . " AND `owner_id`=" . $_SESSION['userId'] . ";");
+        $expiration == "" ? $expiration = NULL : $expiration = str_replace("T", " ", $expiration);
+        $this->runQuery("UPDATE `$this->table` SET `title`=:task_title, `description`=:description, `expiration_date`=:expiration WHERE `id`=:task_id AND `owner_id`=:owner_id;", [
+            ':task_title' => $task_title, 
+            ':description' => $description, 
+            ':expiration' => $expiration,
+            ':task_id' => $task_id,
+            ':owner_id' => $_SESSION['userId']
+        ]);
     }
 
     /**
@@ -41,8 +54,12 @@ class Tasks extends Model {
      */
     public function switchDone($id) {
         $status = $this->isDone($id);
-        $status == 0 ? $status = 1 : $status = 0;
-        $this->runQuery("UPDATE `$this->table` SET `done` = '$status' WHERE `id` = $id AND `owner_id` = " . $_SESSION['userId'] . ";");
+        $status = !$status;
+        $this->runQuery("UPDATE `$this->table` SET `done` = :done WHERE `id` = :id AND `owner_id` = :owner_id;", [
+            ':done' => $status, 
+            ':id' => $id, 
+            ':owner_id' => $_SESSION['userId']
+        ]);
     }
 
     /**
@@ -50,7 +67,10 @@ class Tasks extends Model {
      * @param Int Target task's ID.
      */
     public function remove($id) {
-        $this->runQuery("DELETE FROM `$this->table` WHERE `id` = $id AND `owner_id` = " . $_SESSION['userId'] . ";");
+        $this->runQuery("DELETE FROM `$this->table` WHERE `id` = :id AND `owner_id` = :owner_id;", [
+            ':id' => $id, 
+            ':owner_id' => $_SESSION['userId']
+        ]);
     }
 
     /**
@@ -60,5 +80,15 @@ class Tasks extends Model {
      */
     public function isDone($id) {
         return $this->findById($id)->done;
+    }
+
+    /**
+     * Return the expiration date of the intended task (NULL if no expiration date)
+     *
+     * @param [type] $id task's ID
+     * @return void date-time formated string
+     */
+    public function getExpiration($id) {
+        return $this->findById($id)->expiration_date;
     }
 }
